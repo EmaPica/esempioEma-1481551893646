e0d90305b50c5169e4a7c481f4df5f0c8a4c27dc
EmaPica <emanuela.picardi-cic-it@it.ibm.com>
2016-12-12 15:21:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,121 @@
-/*eslint-env node*/
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+'use strict';
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+// load environment properties from a .env file for local development
+require('dotenv').load({silent: true});
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+var express    = require('express'),
+  app          = express();
+// Bootstrap application settings
+require('./config/express')(app);
 
-// create a new express server
-var app = express();
+//integration with tradeoff analytics service
+var tradeoffAnalyticsConfig = require('./config/tradeoff-analytics-config');
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+tradeoffAnalyticsConfig.setupToken(app, {//for dev purposes. in bluemix it is taken from VCAP.
+  url: process.env.TA_URL || 'https://gateway.watsonplatform.net/tradeoff-analytics/api/v1',
+  username: process.env.TA_USERNAME || 'USERNAME',
+  password: process.env.TA_PASSWORD || 'PASSWORD',
+  version: 'v1'
 });
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    ct: req._csrfToken,
+    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID
+  });
+});
+app.get('/refresh', function(req, res) {
+  refreshData();
+  res.writeHead(200);
+  res.end();
+});
+app.get('/last_refresh', function(req, res) {
+  lastRefresh(function(time){
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.write(time.toJSON());
+    res.end();
+  });
+});
+
+var FILE_RAW = 'config/edmunds/cars_raw.json';
+var FILE_PROBLEM = './public/data/auto.json';
+var edmunds = require('./config/edmunds/Edmunds');
+var fs = require('fs');
+
+var SECOND = 1000,
+  MINUTE= 60*SECOND,
+  HOUR = 60*MINUTE;
+var MAX_TIME_BETWEEN_IMPORTS = 24*HOUR;
+var TIME_BETWEEN_CHECKS = 1*HOUR;
+
+var refreshing= false;
+
+function checkForRefresh(){
+  lastRefresh(function(lastImportTime){
+    var duration = (new Date() - lastImportTime);
+    if(duration>MAX_TIME_BETWEEN_IMPORTS && !refreshing){
+      refreshData();
+    }
+  });
+}
+function refreshData(){
+  if(refreshing){
+    return;
+  }
+  var startTime = Date.now();
+  refreshing = true;
+  function onFailure(err){
+    console.log('import failed. \n'+ err);
+    refreshing = false;
+  }
+  try{
+    edmunds.importEdmunds(function(data){// brings the data from RAW file instead from API
+      fs.writeFile(FILE_RAW, JSON.stringify(data,  null, 2));
+//      var data= JSON.parse(fs.readFileSync(FILE_RAW));
+
+      edmunds.mapEdmunds(data, function(problem){
+        fs.writeFile(FILE_PROBLEM, JSON.stringify(problem,  null, 2));
+        refreshing = false;
+
+        var duration  = (Date.now() - startTime),
+          m= Math.floor(duration/MINUTE),
+          s= Math.floor((duration-m*MINUTE)/SECOND);
+        console.log("Duration: "+m+"M:"+s+"s");
+      });
+    }, onFailure);
+  }catch(e){
+    onFailure(e);
+  }
+    //the server will retry in the next check interval;
+}
+setInterval(checkForRefresh, TIME_BETWEEN_CHECKS);
+
+function lastRefresh(callback){
+  fs.stat(FILE_PROBLEM, function(err, stats){
+    if(stats){//file exist
+      callback(new Date(stats.mtime));
+    }else{
+      callback(new Date(0));
+    }
+  });
+}
+checkForRefresh();
+
+
+module.exports = app;
